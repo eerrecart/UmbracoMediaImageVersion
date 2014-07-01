a4f550fc695cca901e3a21e9380544789a5c6335
eerrecart <errecartelias84@hotmail.com>
2014-07-01 12:25:06 -0300

    add files

---
+++ MediaHelper/Image.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Url.Handler;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Examine;
+using Examine.Providers;
+
+namespace MediaHelper
+{
+    public static class UmbracoDataHelper
+    {
+        static UmbracoHelper helper = new UmbracoHelper(UmbracoContext.Current);
+        static BaseSearchProvider searcher = ExamineManager.Instance.SearchProviderCollection["ExternalSearcher"];
+        
+        public static string GetImageURL(string imageId, string crop)
+        {
+            
+            try
+            {
+                var criteria = searcher.CreateSearchCriteria();
+
+                criteria.Id(int.Parse(imageId));
+
+                var results = searcher.Search(criteria);
+                
+                if (results.Any())
+                {
+                    var result = results.FirstOrDefault();
+
+                    string filename = System.IO.Path.GetFileName(result.Fields["umbracoFile"]);
+
+                    string newfilename = filename.Replace("." + result.Fields["umbracoExtension"], string.Format("_{0}.jpg", crop));
+
+                    string url = result.Fields["umbracoFile"].Replace(filename, newfilename);
+                                    
+                    url += "?v=" + result.Fields["updateDate"];
+
+                    return url;
+
+                }
+
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ UmbracoMediaImageVersion/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UmbracoMediaImageVersion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UmbracoMediaImageVersion")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fd5848b9-3218-4571-8f30-2f98a995bc04")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
